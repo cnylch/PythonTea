202d741b89ef7feaa0ebb41daf87f57ccaec94e5
HISEA <cnylch@163.com>
2017-07-13 16:07:00 +0800

    完成第四章异常处理语句后的测试文件

--- WriteToFiles.py
+++ WriteToFiles.py
@@ -3,30 +3,29 @@ import os
 jack_list = []
 rose_list = []
 try:
-    spoken_data = open('atext.txt')
-    for each_line in spoken_data:
-        try:
-            (role,line_spoken) = each_line.split(':',1)
-            line_spoken = line_spoken.strip() #去除不必要的空格
-            if role == 'JACK':
-                jack_list.append(line_spoken) #将对应回话添加到jack的列表中
-            elif role =='ROSE':
-                rose_list.append(line_spoken)
-        except ValueError:
-            pass
-    spoken_data.close() #读取文件完毕，关闭该文件。
-except IOError:
-    print("The Spoken_data file is missing!")
+    with open('atext.txt') as spoken_data:
+        for each_line in spoken_data:
+            try:
+                (role,line_spoken) = each_line.split(':',1)
+                line_spoken = line_spoken.strip() #去除不必要的空格
+                if role == 'JACK':
+                    jack_list.append(line_spoken) #将对应回话添加到jack的列表中
+                elif role =='ROSE':
+                    rose_list.append(line_spoken)
+            except ValueError:
+                pass
+        spoken_data.close() #读取文件完毕，关闭该文件。
+except IOError as err:
+    print("The Spoken_data file is missing:" + str(err))
 
 try:
-    file_jack = open('file_jack.txt','w')
-    file_rose = open('file_rose.txt','w')
     #使用print 写入到文件当中
-    print(jack_list,file=file_jack)
-    print(rose_list,file=file_rose)
-
-except IOError:
-    print('File IO error!')#文件读取错误，给以提示。
+    with open('file_jack.txt','w') as file_jack:
+        print(jack_list, file=file_jack)
+    with open('file_rose.txt','w') as file_rose:
+        print(rose_list, file=file_rose)
+except IOError as err:
+    print('File IO error!' + str(err))#文件读取错误，给以提示。
 finally:
     #写入文件之后，我们需要关闭对应的文件
     file_jack.close()

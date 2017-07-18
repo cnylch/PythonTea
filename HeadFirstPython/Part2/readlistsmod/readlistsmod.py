10697bfd7b6bf545af1a30022e63ec484e499969
HISEA <cnylch@163.com>
2017-07-18 14:08:10 +0800

    1.1.1 修改了函数在出现数值前进行打印N个制表符的功能

--- HeadFirstPython/Part2/readlistsmod/readlistsmod.py
+++ HeadFirstPython/Part2/readlistsmod/readlistsmod.py
@@ -4,8 +4,9 @@
 它提供了一个read_my_list()的函数，
 本函数可以打印列表，并且
 可以打印嵌套列表。
+第二个参数用了标注遇到嵌套列表时，插入的tab个数。默认为0.
 '''
-def read_my_list(clist):
+def read_my_list(clist,level=0):
     '''
     "clist"参数，可以是一个列表，也可以是一个嵌套的列表
     此函数可以在屏幕上打印每个列表的数据项，包括嵌套列表中的数据项。
@@ -13,6 +14,10 @@ def read_my_list(clist):
     '''
     for each_item in clist :
         if isinstance(each_item,list):
-            read_my_list(each_item)
+            #如遇嵌套列表，则再缩进一个TAB,因此level+1
+            read_my_list(each_item,level+1)
         else:
+            #在出现数据前，打印level个TAB.
+            for tab_num in range(level):
+                print('\t',end='')
             print(each_item)
--- HeadFirstPython/Part2/readlistsmod/setup.py
+++ HeadFirstPython/Part2/readlistsmod/setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 setup(
     name =  'readlistsmod',
-    version = '1.0.0',
+    version = '1.1.1',
     py_modules = ['readlistsmod'],
     author = 'Hisea',
     author_email = 'hiseamail@foxmail.com',
     url = '',
-    description= '对列表进行简单的打印',
+    description= '对列表进行简单的打印，可根据用户需求，'
+                 '在显示一行数据前添加level个TAB，'
+                 '对内嵌列表进行 level+1 个TAB缩进操作。',
 	)
